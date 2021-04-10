9d055fc95089849f1b9e0adb7d6ca91912c0c706
Hanut Singh Gusain <hanutsingh@gmail.com>
2021-04-10 13:00:26 +0530

    Project scaffolded

---
+++ app.js
@@ -0,0 +1,17 @@
+const fastify = require('fastify')({ logger: { prettyPrint: true } });
+
+fastify.get('/', async () => {
+  return { name: "Simple Microservice", uptime: process.uptime() };
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3001, "0.0.0.0")
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start().catch(console.error);
